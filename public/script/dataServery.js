69cabd103a02cc69cc086803d164276b8c12ec65
Fae Daunt <faedaunt@gmail.com>
2016-01-22 00:51:34 +0000

    New text, local rewrite complete

--- public/script/dataServery.js
+++ public/script/dataServery.js
@@ -361,8 +361,10 @@ queueFunc.check = function() {
 	var timer1 = new Date();
 	queueFunc.report(1);
 	var didSend = 0;
-	if (current % 600 <= 1 && deviceData['type'] == 1) {
-		askForNotes();
+	if (current % 300 <= 1 && deviceData['type'] == 1) {
+		setTimeout(function() {
+			askForNotes();	
+		},20000);
 	}
 	while (sendQueue[0] != undefined && sendQueue[0]['timeStamp'] <= current || sendQueue[0] != undefined && sendQueue[0].queueDay < sendQueue[0].userDay ) {
 		didSend = 1;
